91b28664007abd5ba24a869cd7b17a5fa41eeabe
Naasim Shareef <naasim.shareef@diconium.com>
2021-04-28 18:16:50 +0200

    Add gatsby code for blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h2`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
+    <h4>{data.allMarkdownRemark.totalCount}</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <BlogLink to={node.fields.slug}>
+          <BlogTitle>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </BlogTitle>
+        </BlogLink>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
@@ -27,3 +48,26 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields {
+            slug
+          }
+          html
+          excerpt
+        }
+      }
+    }
+  }
+`
