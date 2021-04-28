4ef4c328597c8902c23d54aaf39eb155f1f4ef91
Naasim Shareef <naasim.shareef@diconium.com>
2021-04-28 18:25:33 +0200

    Update posts count text

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <h4>{data.allMarkdownRemark.totalCount}</h4>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
     {data.allMarkdownRemark.edges.map(({ node }) => (
       <div key={node.id}>
         <BlogLink to={node.fields.slug}>
